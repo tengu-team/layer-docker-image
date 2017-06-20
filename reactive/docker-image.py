d47e9e3bd957ce0e0db6a10075382875ceeadf32
sborny <sander.borny@ugent.be>
2017-06-20 13:19:39 +0200

    Added kubernetes private docker repos

--- reactive/docker-image.py
+++ reactive/docker-image.py
@@ -54,20 +54,22 @@ def host_connected(dh_relation):
     log('config.changed.image, generating new UUID')
     container_request = {
         'image': conf.get('image'),
-        'unit': os.environ['JUJU_UNIT_NAME']
+        'unit': os.environ['JUJU_UNIT_NAME'],
+        'docker-registry': conf.get('docker-registry'),
+        'docker-email': conf.get('docker-email')
     }
     log(container_request)
     username = conf.get('username')
-    secret = conf.get('secret')
-    if username != '' or secret != '':
+    password = conf.get('password')
+    if username != '' or password != '':
         if username == '':
-            status_set('blocked', 'If you provide a secret, you should also specify the username.')
+            status_set('blocked', 'If you provide a password, you should also specify the username.')
             return
-        if secret == '':
-            status_set('blocked', 'If you provide a username, you should also set the secret.')
+        if password == '':
+            status_set('blocked', 'If you provide a username, you should also set the password.')
             return
         container_request['username'] = username
-        container_request['secret'] = secret
+        container_request['password'] = password
     ports = conf.get('ports')
     if ports:
         try:
